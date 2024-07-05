044768dc6c65832001ebfa990d84f517a8edb531
ABZB <abzb13@gmail.com>
2024-07-05 15:56:45 -0400

    fixed lots of things, will not crash, also will actually search for correct value

--- multi.py
+++ multi.py
@@ -12,8 +12,9 @@ def multisearch():
 		print(target_value, 'is not an integer, it is ', type(target_value))
 		return
 	
+	low_byte = target_value % 256
+	high_byte = target_value >> 8
 
-	
 	target_directory = askdirectory(title = 'What directory do you want to search?')
 	
 	while True:
@@ -53,23 +54,30 @@ def multisearch():
 			for x in result:
 				if(os.path.isfile(x)):
 					temp_1, temp_2, temp_3 = search_binary_file_two_bytes(target_value, x, '', mode)
-					explicit_address_array = [*explicit_address_array, '\n' + x.replace('/', '_') + '\n', *temp_1]
-					cmp_explicit_array = [*explicit_address_array, '\n' + x.replace('/', '_') + '\n', *temp_2]
-					two_subtractions_array = [*explicit_address_array, '\n' + x.replace('/', '_') + '\n', *temp_3]
+					
+					x = str(x)
+					
+					if(temp_1 != []):
+						explicit_address_array = [*explicit_address_array, '\n' + x + '\n', *temp_1]
+					if(temp_2 != []):
+						cmp_explicit_array = [*cmp_explicit_array, '\n' + x + '\n', *temp_2]
+					if(temp_3 != []):
+						two_subtractions_array = [*two_subtractions_array, '\n' + x + '\n', *temp_3]
 		case 2:
+			explicit_address_array = []
 			cmp_explicit_array = []
 			two_subtractions_array = []
 			for x in result:
 				if(os.path.isfile(x)):
 					print('Now searching: ', x)
 					temp_1, temp_2, temp_3 = search_binary_file_two_bytes(target_value, x, '', mode)
+					
+					x = str(x)
+
 					if(temp_2 != []):
-						cmp_explicit_array = [*explicit_address_array, '\n' + x.replace('/', '_') + '\n', *temp_2]
+						cmp_explicit_array = [*cmp_explicit_array, '\n' + x + '\n', *temp_2]
 					if(temp_3 != []):
-						two_subtractions_array = [*explicit_address_array, '\n' + x.replace('/', '_') + '\n', *temp_3]
-			
-	low_byte = target_value % 256
-	high_byte = target_value >> 8
+						two_subtractions_array = [*two_subtractions_array, '\n' + x + '\n', *temp_3]
 				
 	if(high_byte == 0):
 		output_value_display = hex(low_byte)
@@ -85,13 +93,19 @@ def multisearch():
 			f.write('The following are the hexadecimal addresses where the value ' + output_value_display + ' was found (written explicitly in little-Endian, presumably loaded by functions pointing to that address):\n')
 		
 			for address in explicit_address_array:
-				f.write(str(hex(address)) + '\n')
+				try:	
+					f.write(str(hex(address)) + '\n')
+				except:
+					f.write(str(address) + '\n')
 		
 		#bitshifted cmp function
 		f.write('\nThe following are the hexadecimal addresses where the value ' + output_value_display + ' was found being checked for equality (<low nibble of high + high nibble of low> 0E 5X E3) or (high 0C 5X E3):\n')
         
 		for address in cmp_explicit_array:
-			f.write(str(hex(address)) + '\n')
+			try:
+				f.write(str(hex(address)) + '\n')
+			except:
+				f.write(str(address) + '\n')
 			
 		#regular cmp function (one byte) or sub followed by subs (two bytes)
 		if(high_byte == 0):
@@ -100,6 +114,9 @@ def multisearch():
 			f.write('\nThe following are the hexadecimal addresses where the value ' + output_value_display + ' was found being checked for equality (high XX 4X E2 low XX 5X):\n')
 		
 		for address in two_subtractions_array:
-			f.write(str(hex(address)) + '\n')
+			try:
+				f.write(str(hex(address)) + '\n')
+			except:
+				f.write(str(address) + '\n')
 			
 multisearch()
